d57f2bbd9892669ba76af04c1d9c83f259990868
Daniel <daniel_andres.03@outlook.com>
2021-05-25 09:24:07 -0500

    implementacion try catch para que no colapce

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,18 +4,29 @@ const pool = require('../database');
 
 router.get('/', async (req, res) => {
     const { placa, fecha } = req.body;
-    const consulta = await pool.query('SELECT * FROM registro WHERE placa = ? AND fecha = ?', [placa,fecha]);
-    if (consulta!=null) {
-        res.sendStatus(404)
-    }else{
-        res.sendStatus(200);
+    try {
+        const consulta = await pool.query('SELECT * FROM registro WHERE placa = ? AND fecha = ?', [placa, fecha]);
+        if (consulta != null) {
+            res.sendStatus(404)
+        } else {
+            res.sendStatus(200);
+        }
+    } catch (error) {
+        res.sendStatus(500);
     }
+
+
 });
 
 router.post('/', async (req, res) => {
     const { placa } = req.body;
-    await pool.query('INSERT INTO registro set ?', placa);
-    req.flash('success', 'registro correcto');
+    try {
+        await pool.query('INSERT INTO registro (placa) VALUES (?)', placa);
+        res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
 });
 
 module.exports = router;
