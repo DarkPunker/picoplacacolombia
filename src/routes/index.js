d9044b16d43700ca96f258f1d2fb97285ba21f9e
Daniel <daniel_andres.03@outlook.com>
2021-05-24 23:56:10 -0500

    correccion index ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,11 @@ const pool = require('../database');
 router.get('/', async (req, res) => {
     const { placa, fecha } = req.body;
     const consulta = await pool.query('SELECT * FROM registro WHERE placa = ? AND fecha = ?', [placa,fecha]);
-    if (consulta[0]!=null) res.send(0);
-    res.send(1);
+    if (consulta!=null) {
+        res.sendStatus(404)
+    }else{
+        res.sendStatus(200);
+    }
 });
 
 router.post('/', async (req, res) => {
