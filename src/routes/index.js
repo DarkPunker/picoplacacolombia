96f02cf763c431800fc3291d948aec9759c80014
Daniel <daniel_andres.03@outlook.com>
2021-05-25 11:08:30 -0500

    validacion de la fecha

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,20 +2,31 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
+function getDateTime() {
+
+    var date = new Date();
+
+    date.setHours(0);
+    date.setSeconds(0);
+    date.setMinutes(0);
+    date.setMilliseconds(0);
+    
+    return date;
+}
+
 router.get('/', async (req, res) => {
-    const { placa, fecha } = req.body;
+    const { placa } = req.body;
     try {
-        const consulta = await pool.query('SELECT * FROM registro WHERE placa = ? AND fecha = ?', [placa, fecha]);
-        if (consulta != null) {
-            res.sendStatus(404)
+        const consulta = await pool.query('SELECT * FROM registro WHERE placa = ? AND fecha >= ?', [placa, getDateTime() ]);
+        if (consulta[0] != null) {
+            res.sendStatus(200)
         } else {
-            res.sendStatus(200);
+            res.sendStatus(404);
         }
+        /* console.log(consulta); */
     } catch (error) {
         res.sendStatus(500);
     }
-
-
 });
 
 router.post('/', async (req, res) => {
