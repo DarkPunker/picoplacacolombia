7fa59858c60a91c4239bb282875640c21079adf5
Daniel <daniel_andres.03@outlook.com>
2021-05-25 23:08:31 -0500

    correccion modifiacion de fecha

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const pool = require('../database');
 function getDateTime() {
 
     var date = new Date();
-
+    
     date.setHours(0);
     date.setSeconds(0);
     date.setMinutes(0);
@@ -18,12 +18,12 @@ router.get('/:placa', async (req, res) => {
     const { placa } = req.params;
     try {
         const consulta = await pool.query('SELECT * FROM registro WHERE placa = ? AND fecha >= ?', [placa, getDateTime() ]);
+        //console.log(consulta);
         if (consulta[0] != null) {
             res.sendStatus(200)
         } else {
             res.sendStatus(404);
         }
-        /* console.log(consulta); */
     } catch (error) {
         res.sendStatus(500);
     }
@@ -32,8 +32,9 @@ router.get('/:placa', async (req, res) => {
 router.post('/', async (req, res) => {
     const { placa } = req.body;
     try {
-        await pool.query('INSERT INTO registro (placa) VALUES (?)', placa);
-        res.sendStatus(200);
+        const consul = await pool.query('INSERT INTO registro (placa) VALUES (?)', placa);
+        await pool.query('UPDATE registro SET fecha = DATE_SUB(fecha, INTERVAL 5 HOUR) WHERE idregistro = ?', consul.insertId);
+        //res.sendStatus(200);
     } catch (error) {
         res.sendStatus(500);
     }
