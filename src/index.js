5b85b22ebbbfda194b3b12e1b63a8cd48969d060
Daniel <daniel_andres.03@outlook.com>
2021-05-31 20:15:04 -0500

    modificacion y eliminar conecciones con Handlebars

--- src/index.js
+++ src/index.js
@@ -17,16 +17,6 @@ require('./lib/passport');
 
 //settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs',
-    helpers: require('./lib/handlebars')
-});
-app.engine('.hbs', hbs.engine);
-app.set('view engine', '.hbs');
 
 //middlewares
 app.use(session({
