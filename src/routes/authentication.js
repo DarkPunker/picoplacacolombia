9bf84b312b77877cbd964d5e191b2ccb16385b69
Daniel <daniel_andres.03@outlook.com>
2021-06-01 01:48:33 -0500

    arreglo en signin

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,38 +2,41 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const pool = require('../database');
+const helpers = require('../lib/helpers');
 
 const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
-router.post('/signup', /* isNotLoggedIn, */ async (req, res) => {
+router.post('/signup', /* isNotLoggedIn, */ passport.authenticate('local.signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+router.post('/signin', /* isNotLoggedIn, */ async (req, res, next) => {
     const { nombreUsuario, contrasena } = req.body;
-    const rows = await pool.query('SELECT * FROM usuario WHERE nombreUsuario = ?', nombreUsuario);
-    console.log(rows[0]);
-    if (rows[0] != null) {
-        const valiPassword = await helpers.matchPassword(contrasena, rows[0].contrasena);
-        if (valiPassword) {
-            if (rows[0].estado != 0) {
-                res.json({
-                    nombreUsuario: rows[0].nombreUsuario,
-                    nombreEstacion: rows[0].nombreEstacion
-                });
+    try {
+        const rows = await pool.query('SELECT * FROM usuario WHERE nombreUsuario = ?', nombreUsuario);
+        if (rows[0] != null) {
+            const valiPassword = await helpers.matchPassword(contrasena, rows[0].contrasena);
+            if (valiPassword) {
+                if (rows[0].estado != 0) {
+                    res.json({
+                        nombreUsuario: rows[0].nombreUsuario,
+                        nombreEstacion: rows[0].nombreEstacion
+                    });
+                } else {
+                    res.sendStatus(404);
+                }
             } else {
                 res.sendStatus(404);
             }
         } else {
             res.sendStatus(404);
         }
-    } else {
-        res.sendStatus(404);
+    } catch (error) {
+        res.sendStatus(500);
     }
-});
 
-router.post('/signin', /* isNotLoggedIn, */ (req, res, next) => {
-    passport.authenticate('local.signin', {
-        successRedirect: '/',
-        failureRedirect: '/signin',
-        failureFlash: true
-    })(req, res, next);
 });
 
 router.get('/logout', (req, res) => {
